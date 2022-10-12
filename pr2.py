65f15804f34508b1a8b4a45d9155236cb3520ded
Desktop <vit10032@gmail.com>
2022-10-12 16:50:15 +0300

    1

--- pr2.py
+++ pr2.py
@@ -1,8 +1,12 @@
 import random
-
+j=0
 a = int(input('Введите количество человек: '))
 p = int((a*(a-1))/2)
 print ('Количество пар: ', p)
-for i in range(p):
-    d=random.randint(0, 365)
-    print(d)
+for n in range (0, 10000):
+    for i in range(p):
+        d=random.randint(1, 365)
+        if d==1:
+            j=j+1
+            break
+print ('Вероятность того, что в группе есть совпадения дней рождения равна',j/100,'%')
